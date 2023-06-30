3f9b2320f5603fc462ca0a680c8020bd706a29ca
Trajce Prodanov <trajce.p@live.com>
2023-07-01 01:14:31 +0200

    github pages support

--- README.md
+++
@@ -1,3 +0,0 @@
-# developer-portfolio
-
-A personal portfolio built using React.js, TailwindCSS and some npm packages.
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="text-neutral-500 flex justify-center my-4 text-xs w-screen">
-      <p>&copy; All rights reserved - Trajche Prodanov</p>
-    </footer>
-  );
-};
-
-export default Footer;
